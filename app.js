8adc1f8c1044c9d0b04d9ce4f82a30a03878e292
EnigMoiD <evan.dorsky@icloud.com>
2013-12-12 13:11:54 -0500

    Basic node webserver, stylus, jade, zepto

---
+++ app.js
@@ -0,0 +1,36 @@
+var
+express = require('express'),
+app = express(),
+stylus = require('stylus'),
+nib = require('nib');
+
+app.set('views', __dirname + '/views');
+
+app.configure(function() {
+	app.use(stylus.middleware({
+			src: __dirname + '/views', 
+			dest: __dirname + '/public', 
+			compile: compile,
+			force: true
+		})
+	);	
+	app.use(express.static(__dirname + '/public'));
+})
+
+app.set('view engine', 'jade');
+
+function compile(str, path) {
+	return stylus(str)
+		.set('filename', path)
+		.use(nib());
+}
+
+app.get('/', function (req, res) {
+	res.render('index', {
+		title : 'Home'
+	})
+});
+
+app.listen(8080);
+
+console.log('> running on port 8080');
